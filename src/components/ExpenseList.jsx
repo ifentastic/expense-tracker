5a69c747ca423d4489e40b6eec93a94d98f69f39
ifentastic <adedo4242@gmail.com>
2024-08-28 12:43:43 -0400

    list of expenses

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,12 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
 
-export default function ExpenseList() {
+export default function ExpenseList({ setItems }) {
     // Dummy list of expenses
     const expenses = [
         { id: 1, name: "Shopping", cost: 40 },
         { id: 2, name: "Car Service", cost: 400 },
         { id: 3, name: "Salaries", cost: 4000 }
     ];
+
+    function removeExpenseHandler(id) {
+        setItems((prevItems) => prevItems.filter(item => item.id !== id));
+    };
 
     return (
         // Iterate over expense list and display an expense item
@@ -16,7 +20,8 @@ export default function ExpenseList() {
                 key={expense.id} 
                 id={expense.id} 
                 name={expense.name} 
-                cost={expense.cost} 
+                cost={expense.cost}
+                onRemove={() => removeExpenseHandler(expense.id)} 
             />
         ))}
        </ul>
