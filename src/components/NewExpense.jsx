5f790b6331cfa8f43f553668c4f7b5ace8943442
ifentastic <adedo4242@gmail.com>
2024-08-28 12:48:37 -0400

    updated file

---
+++ src/components/NewExpense.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import AddExpenseForm from "./AddExpenseForm";
+
+export default function NewExpense({ onAddExpense }) {
+    // Manage state of editing data
+    const [isEditing, setIsEditing] = useState(false);
+
+    function saveExpenseDataHandler(enteredExpenseData) {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        onAddExpense(expenseData);
+        setIsEditing(false);
+    }
+    
+    function startEditingHandler() {
+        setIsEditing(true);
+    }
+
+    function stopEditingHandler() {
+        setIsEditing(false);
+    }
+
+    return (
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <div>
+                {!isEditing && (
+                    <button 
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold p-4 m-8 w-64 max-w-full rounded-xl text-center"
+                        onClick={startEditingHandler}
+                    >
+                        Add Expense
+                    </button>
+                )}
+                {isEditing && (
+                    <AddExpenseForm 
+                        onSaveExpenseData={saveExpenseDataHandler}
+                        onCancel={stopEditingHandler}
+                    />
+                )}
+            </div>
+        </div>    
+    );
+}
