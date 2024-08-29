18930397ee4bca9ecdffc4149d44ddea139719e2
ifentastic <adedo4242@gmail.com>
2024-08-29 15:59:08 -0400

    adjusted padding

--- src/components/Remaining.jsx
+++ src/components/Remaining.jsx
@@ -1,8 +1,7 @@
 export default function Remaining() {
     return (
         <div 
-            className="relative px-3 py-3 mb-4 border rounded bg-green-200 border-green-300 text-green-800"
-            role="alert"
+            className="relative px-3 py-4 mb-4 pb-4 items-center border rounded bg-green-200 border-green-300 text-green-800"
         > {/* green background */}
             <span>Remaining: $200</span>
         </div>
