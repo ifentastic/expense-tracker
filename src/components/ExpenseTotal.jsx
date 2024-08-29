2dc2c3f037c5e2ac21548d220bc44af83e354afe
ifentastic <adedo4242@gmail.com>
2024-08-29 15:48:58 -0400

    formatted form

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -1,8 +1,7 @@
 export default function ExpenseTotal() {
     return (
         <div
-            className="relative px-3 py-3 mb-4 border rounded text-blue-800 bg-blue-200 border-blue-300"
-            role="alert"
+            className="relative px-3 py-4 mb-4 pb-4 border rounded text-blue-800 bg-blue-200 border-blue-300"
         > {/* blue background */}
             <span>Spent so far: $1800</span>
         </div>
