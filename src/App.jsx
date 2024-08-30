224a5b5b72fbb6b5469fc2781929b4e29ad730e4
ifentastic <adedo4242@gmail.com>
2024-08-29 21:45:35 -0400

    adding props to components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import Budget from "./components/Budget";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseTotal from "./components/ExpenseTotal";
 import NewExpense from "./components/NewExpense";
-import Remaining from "./components/Remaining";
+import RemainingBudget from "./components/RemainingBudget";
 
 function App() {
+  // Dummy list of expenses as initial state
+  const [expenses, setExpenses] = useState([
+    { id: 1, name: "Shopping", cost: 40 },
+    { id: 2, name: "Car Service", cost: 400 },
+    { id: 3, name: "Salaries", cost: 1400 }
+  ]);
+  
   const [budget, setBudget] = useState(2000);
 
   function handleSaveClick(value) {
     setBudget(value);
+  }
+
+  function addExpenseHandler(expense) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, expense];
+    })
   }
   
   return (
@@ -22,20 +35,20 @@ function App() {
           <Budget budget={budget} handleSaveClick={handleSaveClick} />
         </div>
         <div className="relative sm:flex-grow sm:flex-1">
-          <Remaining />
+          <RemainingBudget expenses={expenses} budget={budget} />
         </div>
         <div className="relative sm:flex-grow sm:flex-1">
-          <ExpenseTotal />
+          <ExpenseTotal expenses={expenses} />
         </div>  
       </div>
       <h3 className="mt-3">Expenses</h3>
       <div className="flex flex-wrap mt-3">
         <div className="relative sm:flex-grow sm:flex-1">
-          <ExpenseList />
+          <ExpenseList expenses={expenses} setExpenses={setExpenses} />
         </div>
       </div>
       <div className="relative sm:flex-grow sm:flex-1">
-          <NewExpense />
+          <NewExpense onAddExpense={addExpenseHandler} />
       </div>
     </div>
   );
