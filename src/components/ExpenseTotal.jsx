dafc2c152103d73bc9d8d72b1578d691394055a8
ifentastic <adedo4242@gmail.com>
2024-08-29 21:40:49 -0400

    outputting value dynamically

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -1,9 +1,13 @@
-export default function ExpenseTotal() {
+export default function ExpenseTotal({ expenses }) {
+    const total = expenses.reduce((total, item) => {
+        return total += item.cost;
+    }, 0);
+    
     return (
         <div
             className="relative px-3 py-4 mb-4 pb-4 border rounded text-blue-800 bg-blue-200 border-blue-300"
         > {/* blue background */}
-            <span>Spent so far: $1800</span>
+            <span>Spent so far: ${total}</span>
         </div>
     );
 }
