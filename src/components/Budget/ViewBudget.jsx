d9a9d808ae913e394fd60532af79524c20075439
ifentastic <adedo4242@gmail.com>
2024-09-01 20:44:24 -0400

    logic to view budget

---
+++ src/components/Budget/ViewBudget.jsx
@@ -0,0 +1,14 @@
+export default function ViewBudget({ budget, handleEditClick }) {
+    return (
+        <>
+            <span>Budget: ${budget}</span>
+            <button
+                type="button"
+                className="inline-block align-middle text-center select-none border-none font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600"
+                onClick={handleEditClick}
+            >
+                Edit
+            </button>
+        </>
+    );
+}
