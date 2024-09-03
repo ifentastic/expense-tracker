0a7ddbaaa4351180b64b5556f9522c393b48f357
ifentastic <adedo4242@gmail.com>
2024-09-03 07:57:21 -0400

    modified button appearance

--- src/components/Budget/EditBudget.jsx
+++ src/components/Budget/EditBudget.jsx
@@ -19,7 +19,7 @@ export default function EditBudget({ budget, onSave }) {
             />
             <button
                 type="button"
-                className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600"
+                className="inline-block align-middle text-center select-none border-none font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600"
                 onClick={() => onSave(value)}
             >
                 Save
