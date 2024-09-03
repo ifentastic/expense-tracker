c47237b83b9fe98cba7ac0b1f8e2839fc0f106b7
ifentastic <adedo4242@gmail.com>
2024-09-03 07:55:57 -0400

    using context

--- src/components/Budget/Budget.jsx
+++ src/components/Budget/Budget.jsx
@@ -1,29 +1,34 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ExpenseContext } from "../context/ExpenseContext";
 import EditBudget from "./EditBudget";
 import ViewBudget from "./ViewBudget";
 
-export default function Budget({ budget, handleSaveClick }) {
+export default function Budget() {
+    const { budget, dispatch } = useContext(ExpenseContext);
+    
     // Manage editing state
     const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
+    function handleEdit() {
         setIsEditing(true);
     }
 
     function handleSave(value) {
-        handleSaveClick(value);
+        dispatch({
+            type: "EDIT_BUDGET",
+            payload: value
+        });
         setIsEditing(false);
     }
     
     return (
         <div 
-            className="relative px-3 py-3 mb-4 border rounded bg-gray-300 border-gray-400 text-gray-800 p-6 flex items-center justify-between" 
-            role="alert"
+            className="relative px-3 py-3 mb-4 border rounded bg-gray-300 border-gray-400 text-gray-800 p-6 flex items-center justify-between"
         > {/* gray background */}
             {isEditing ? (
                 <EditBudget onSave={handleSave} budget={budget} />
             ) : (
-                <ViewBudget handleEditClick={handleEditClick} budget={budget} />
+                <ViewBudget onEdit={handleEdit} budget={budget} />
             )}
         </div>
     );
