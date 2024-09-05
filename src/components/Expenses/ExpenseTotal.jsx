d7d8c8d75c83a2fbcceb8bdc03987bb2dfe22f00
ifentastic <adedo4242@gmail.com>
2024-09-05 15:55:20 -0400

    calculating total amount spent

--- src/components/Expenses/ExpenseTotal.jsx
+++ src/components/Expenses/ExpenseTotal.jsx
@@ -4,14 +4,10 @@ import { ExpenseContext } from "../context/ExpenseContext";
 export default function ExpenseTotal() {
     const { expenses } = useContext(ExpenseContext);
     
-    const total = expenses.reduce((total, item) => {
-        return total += item.cost;
-    }, 0);
+    const total = expenses.reduce((total, item) => total + item.cost, 0);
     
     return (
-        <div
-            className="relative px-3 py-4 mb-4 pb-4 border rounded text-blue-800 bg-blue-200 border-blue-300"
-        > {/* blue background */}
+        <div className="relative px-3 py-4 mb-4 pb-4 border rounded text-blue-800 bg-blue-200 border-blue-300">
             <span>Spent so far: ${total}</span>
         </div>
     );
