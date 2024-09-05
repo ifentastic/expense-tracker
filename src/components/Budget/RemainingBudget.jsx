b5fd59b074f4a20abf99567607ba8b80cd15fa49
ifentastic <adedo4242@gmail.com>
2024-09-05 15:52:34 -0400

    calculating remaining amount

--- src/components/Budget/RemainingBudget.jsx
+++ src/components/Budget/RemainingBudget.jsx
@@ -4,16 +4,12 @@ import { ExpenseContext } from "../context/ExpenseContext";
 export default function RemainingBudget() {
     const { expenses, budget } = useContext(ExpenseContext);
     
-    const totalExpenses = expenses.reduce((total, item) => {
-        return total += item.cost;
-    }, 0);
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
 
     const alertType = totalExpenses > budget ? "bg-red-200 border-red-300 text-red-800" : "bg-green-200 border-green-300 text-green-800";
     
     return (
-        <div 
-            className={`relative px-3 py-4 mb-4 pb-4 items-center border rounded ${alertType}`}
-        > {/* green or red background depending on whether the total anmount of expenses exceeds the budget */}
+        <div className={`relative px-3 py-4 mb-4 pb-4 items-center border rounded ${alertType}`}>
             <span>Remaining: ${budget - totalExpenses}</span>
         </div>
     );
