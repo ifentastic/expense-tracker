5604d6bc5691ae76e522d512e9fb33bfa245ae2a
ifentastic <adedo4242@gmail.com>
2024-09-05 15:54:30 -0400

    converting cost to a number

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -31,7 +31,7 @@ export default function ExpenseItem({ id, name, cost }) {
     function handleSaveExpense() {
         dispatch({
             type: "EDIT_EXPENSE",
-            payload: { id, name: editedName, cost: editedCost }
+            payload: { id, name: editedName, cost: +editedCost }
         });
         setIsEditing(false);
     }
@@ -47,33 +47,33 @@ export default function ExpenseItem({ id, name, cost }) {
         {name} {/* List expense item */}
         {isEditing ? (
             <div>
-                <input
-                    type="text"
-                    value={editedName}
-                    onChange={handleEditName}
-                    placeholder="Expense Name"
-                />
-                <input
-                    type="number"
-                    value={editedCost}
-                    onChange={handleEditCost}
-                    placeholder="Expense Cost"
-                />
-                <button
-                    className="bg-gray-500 hover:bg-gray-600 text-white font-bold px-4 py-2 rounded"
-                    onClick={handleCancelEdit}
-                >
-                    Cancel
-                </button>
-                <button
-                    className="bg-green-500 hover:bg-green-600 text-white font-bold px-4 py-2 rounded"
-                    onClick={handleSaveExpense}
-                >
-                    Save
-                </button>
-            </div>
+            <input
+                type="text"
+                value={editedName}
+                onChange={handleEditName}
+                placeholder="Expense Name"
+            />
+            <input
+                type="number"
+                value={editedCost}
+                onChange={handleEditCost}
+                placeholder="Expense Cost"
+            />
+            <button
+                className="bg-gray-500 hover:bg-gray-600 text-white font-bold px-4 py-2 rounded mr-3"
+                onClick={handleCancelEdit}
+            >
+                Cancel
+            </button>
+            <button
+                className="bg-green-500 hover:bg-green-600 text-white font-bold px-4 py-2 rounded"
+                onClick={handleSaveExpense}
+            >
+                Save
+            </button>
+        </div>
         ) : (
-           <div className="flex justify-end items-center">
+            <div className="flex justify-end items-center">
                 <span className="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded bg-blue-500 text-white hover:bg-blue-600 py-1 px-3 mr-3">
                     ${cost} {/* Cost of the expense */}
                 </span>
