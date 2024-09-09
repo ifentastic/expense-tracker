95467eedb8fe4334d72504657ca35b7cc3f209e9
ifentastic <adedo4242@gmail.com>
2024-09-09 19:20:42 -0400

    changed button hover color

--- src/components/Budget/ViewBudget.jsx
+++ src/components/Budget/ViewBudget.jsx
@@ -4,7 +4,7 @@ export default function ViewBudget({ budget, onEdit }) {
             <span>Budget: ${budget}</span>
             <button
                 type="button"
-                className="inline-block align-middle text-center select-none border-none font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600"
+                className="inline-block align-middle text-center select-none border-none font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-500 text-white hover:bg-blue-600"
                 onClick={onEdit}
             >
                 Edit
