b22f14ab886bb16e5c76a0fe551eb7f702843321
ifentastic <adedo4242@gmail.com>
2024-09-22 23:20:39 -0400

    remove commented code

--- src/components/Expenses/ExpenseList.jsx
+++ src/components/Expenses/ExpenseList.jsx
@@ -17,44 +17,6 @@ export default function ExpenseList() {
         );
         setFilteredExpenses(searchResults.length > 0 ? searchResults : expenses);
 	}
-    
-    // return (
-    //     <>
-    //        <input
-    //             type="text"
-    //             className="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded mb-2 sm:mr-2"
-    //             placeholder="Search an expense..."
-    //             onChange={handleChange}
-    //         />
-            
-    //         {/* Iterate over expense list and display an expense expense if there are search results */}
-    //         {/* Otherwise display a message */}
-    //         {filteredExpenses.length > 0 ? (
-    //             <table className="w-full max-w-full mb-4 bg-transparent">
-    //                 <thead>
-    //                     <tr>
-    //                         <th scope="col">Description</th>
-    //                         <th scope="col">Cost</th>
-    //                         <th scope="col">Category</th>
-    //                     </tr>
-    //                 </thead>
-    //                 <tbody>
-    //                     {filteredExpenses.map((expense) => (
-    //                         <Expenseexpense
-    //                             key={expense.id}
-    //                             id={expense.id}
-    //                             description={expense.description}
-    //                             cost={expense.cost}
-    //                             category={expense.category}
-    //                         />
-    //                     ))}
-    //                 </tbody>
-    //             </table>
-    //         ) : (
-    //             <p className="text-black text-center">No expenses found.</p>
-    //         )}            
-    //     </>    
-    // );
 
     return (
         <>
@@ -93,10 +55,7 @@ export default function ExpenseList() {
                         />
                     ))}
                 </tbody>
-            </table> 
-            
-            
-        </>
-        
+            </table>             
+        </>       
     );
 }
