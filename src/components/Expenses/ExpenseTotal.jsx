edf0d398eb7baa4333da33696236bad4cbea2148
ifentastic <adedo4242@gmail.com>
2025-04-05 19:59:26 -0400

    format total expenses

--- src/components/Expenses/ExpenseTotal.jsx
+++ src/components/Expenses/ExpenseTotal.jsx
@@ -4,11 +4,15 @@ import { ExpenseContext } from "../context/ExpenseContext";
 export default function ExpenseTotal() {
     const { expenses } = useContext(ExpenseContext);
     
-    const total = expenses.reduce((total, item) => total + item.cost, 0);
+    // Convert cost values to numbers and calculate total expenses
+    const totalExpenses = expenses.reduce((total, item) => total + parseFloat(item.cost), 0);
+
+    // Format total expenses to 2 decimal places
+    const formattedTotalExpenses = totalExpenses.toFixed(2);
     
     return (
         <div className="relative px-3 py-4 mb-4 pb-4 border rounded text-blue-800 bg-blue-200 border-blue-300">
-            <span>Spent so far: ${total}</span>
+            <span>Spent so far: ${formattedTotalExpenses}</span>
         </div>
     );
 }
