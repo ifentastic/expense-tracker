f38f13fd028a53220a1771dc3a919debe3aa32ce
ifentastic <adedo4242@gmail.com>
2025-04-05 19:51:11 -0400

    format remaining amount

--- src/components/Budget/RemainingBudget.jsx
+++ src/components/Budget/RemainingBudget.jsx
@@ -4,13 +4,18 @@ import { ExpenseContext } from "../context/ExpenseContext";
 export default function RemainingBudget() {
     const { expenses, budget } = useContext(ExpenseContext);
     
-    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+    // Convert cost values to numbers and calculate total expenses
+    const totalExpenses = expenses.reduce((total, item) => total + parseFloat(item.cost), 0);
 
+    // Calculate remaining budget and format it to 2 decimal places
+    const remainingBudget = (budget - totalExpenses).toFixed(2);
+
+    // Conditional background color rendering based on remaining amount
     const alertType = totalExpenses > budget ? "bg-red-200 border-red-300 text-red-800" : "bg-green-200 border-green-300 text-green-800";
     
     return (
         <div className={`relative px-3 py-4 mb-4 pb-4 items-center border rounded ${alertType}`}>
-            <span>Remaining: ${budget - totalExpenses}</span>
+            <span>Remaining: ${remainingBudget}</span>
         </div>
     );
 }
